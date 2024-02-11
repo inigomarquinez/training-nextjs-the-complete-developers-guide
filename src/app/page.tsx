639a136432412daaace13b6aeb71b7d4b4394e93
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-11 16:51:09 +0100

    feat: add Hero component and update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
 
-import homeImage from "/public/home.jpg";
+import Hero from "@/components/hero";
+import homeImg from "/public/home.jpg";
 
 export default function Home() {
   return (
-    <div>
-      Home page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          fill
-          alt="car factory"
-          src={homeImage}
-          style={{ objectFit: "cover" }}
-          />
-        </div>
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
