c7db021d967ce923053c4e80b4373dafe4a53630
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-11 16:34:50 +0100

    feat: add home cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
+import homeImage from "/public/home.jpg";
+
 export default function Home() {
   return (
     <div>
       Home page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          fill
+          alt="car factory"
+          src={homeImage}
+          style={{ objectFit: "cover" }}
+          />
+        </div>
     </div>
   );
 }
