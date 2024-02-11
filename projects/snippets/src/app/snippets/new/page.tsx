648c21f1347aac8f565681e0f7e52011872d20f9
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-11 20:26:09 +0100

    feat: update SnippetCreatePage to include server-side action

--- projects/snippets/src/app/snippets/new/page.tsx
+++ projects/snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,27 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action!
+    'use server';
+
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    redirect('/');
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
