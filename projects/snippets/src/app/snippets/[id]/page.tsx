28334f5dff24767c9702f795bd7528863d40d072
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-12 08:34:30 +0100

    feat: add edit and delete buttons to snippet show page

--- projects/snippets/src/app/snippets/[id]/page.tsx
+++ projects/snippets/src/app/snippets/[id]/page.tsx
@@ -24,7 +24,20 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
   return (
     <div>
-      {snippet.title}
+      <div className='flex m-4 justify-between items-center'>
+        <h1 className='text-xl font-bold'>
+          {snippet.title}
+        </h1>
+        <div className='flex gap-4'>
+          <button className='p-2 border rounded'>Edit</button>
+          <button className='p-2 border rounded'>Delete</button>
+        </div>
+      </div>
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+        <code>
+          {snippet.code}
+        </code>
+      </pre>
     </div>
   )
 }
