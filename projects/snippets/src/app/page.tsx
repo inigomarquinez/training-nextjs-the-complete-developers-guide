63a17ce5ce7bbe649fa85e0a910d093d387d3856
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-12 08:27:03 +0100

    feat: refactor snippet rendering and add navigation links

--- projects/snippets/src/app/page.tsx
+++ projects/snippets/src/app/page.tsx
@@ -1,11 +1,34 @@
+import Link from 'next/link';
+
 import { db } from '@/db';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        className='flex items-center justify-between p-2 border rounded'
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className='flex m-2 justify-between items-center'>
+        <h1 className='text-xl font-bold'>Snippets</h1>
+        <Link href='/snippets/new' className='p-2 border rounded'>
+          New
+        </Link>
+      </div>
+      <div className='flex flex-col gap-2'>
+        {renderedSnippets}
+      </div>
+    </div>
+  );
 }
