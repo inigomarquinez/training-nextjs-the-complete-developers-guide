8fd495c04fecff96e18cf5f1730ad134b2327f35
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-12 08:17:33 +0100

    feat: add fetch delay in SnippetShowPage

--- projects/snippets/src/app/snippets/[id]/page.tsx
+++ projects/snippets/src/app/snippets/[id]/page.tsx
@@ -9,6 +9,9 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  // Simulate fetch delay
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const snippet = await db.snippet.findFirst({
     where: {
       id: parseInt(props.params.id),
