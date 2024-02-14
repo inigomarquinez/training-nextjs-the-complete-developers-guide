8fcd0c430ed04ad5e49b8140cbbca02dffa8c903
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-14 19:27:40 +0100

    feat: add snippet edit form with server action

--- projects/snippets/src/app/snippets/[id]/edit/page.tsx
+++ projects/snippets/src/app/snippets/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
-import { parse } from 'path';
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+
+import SnippetEditForm from '@/components/snippetEditForm';
 
 interface SnippetEditPageProps {
   params: {
@@ -10,16 +11,17 @@ interface SnippetEditPageProps {
 
 export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const id = parseInt(props.params.id);
-
   const snippet = await db.snippet.findFirst({
-    where: {
-      id,
-    },
+    where: { id },
   });
 
   if (!snippet) {
     return notFound();
   }
 
-  return <div>Editing snippet with title {snippet.title}</div>;
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 }
