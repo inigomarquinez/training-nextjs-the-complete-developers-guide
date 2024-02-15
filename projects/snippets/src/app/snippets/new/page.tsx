521b6819eed0a8eb467af18078e9f8646b586058
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-15 08:49:42 +0100

    refactor: refactor createSnippet function

--- projects/snippets/src/app/snippets/new/page.tsx
+++ projects/snippets/src/app/snippets/new/page.tsx
@@ -1,27 +1,15 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-import { db } from '@/db';
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server action!
-    'use server';
-
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    redirect('/');
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: '',
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -45,6 +33,12 @@ export default function SnippetCreatePage() {
             id="code"
           />
         </div>
+
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
 
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
