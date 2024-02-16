195e2bc2c5a2e9f10e1c8277fd25642842311855
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-16 08:49:53 +0100

    feat: add revalidation and redirection after snippet edit

--- projects/snippets/src/actions/index.ts
+++ projects/snippets/src/actions/index.ts
@@ -11,6 +11,7 @@ export async function editSnippet(id: number, code: string) {
     data: { code },
   });
 
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
 
--- projects/snippets/src/app/snippets/[id]/page.tsx
+++ projects/snippets/src/app/snippets/[id]/page.tsx
@@ -48,3 +48,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => ({ id: snippet.id.toString() }));
+}
