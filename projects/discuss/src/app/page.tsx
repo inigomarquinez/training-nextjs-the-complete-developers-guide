1b8b8b18738a83a4898a5daeaf73282b3f4e08f6
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-19 08:23:18 +0100

    feat: add sign-in and sign-out from server component

--- projects/discuss/src/app/page.tsx
+++ projects/discuss/src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from "@nextui-org/react";
 
-export default function Home() {
+import { auth } from "@/auth";
+import * as actions from "@/actions";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+
+      {
+        session?.user ? (
+          <pre>{JSON.stringify(session.user, null, 2)}</pre>
+        ) : (
+          <div>Signed out</div>
+        )
+      }
     </div>
   );
 }
