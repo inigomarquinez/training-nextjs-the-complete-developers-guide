90bffb3f0f5c4af3fdb148b00822cedcfd4070a2
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-24 13:47:29 +0100

    feat: add authentication check and error handling in createTopic function

--- projects/discuss/src/actions/create-topic.ts
+++ projects/discuss/src/actions/create-topic.ts
@@ -1,12 +1,13 @@
 'use server';
 
 import { z } from 'zod';
+import { auth } from '@/auth';
 
 const createTopicSchema = z.object({
   name: z
     .string()
     .min(3)
-    .regex(/^[a-z-]+$/, {
+    .regex(/[a-z-]/, {
       message: 'Must be lowercase letters or dashes without spaces',
     }),
   description: z.string().min(10),
@@ -16,6 +17,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 
@@ -31,6 +33,15 @@ export async function createTopic(
   if (!result.success) {
     return {
       errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this.'],
+      },
     };
   }
 
--- projects/discuss/src/components/topics/topic-create-form.tsx
+++ projects/discuss/src/components/topics/topic-create-form.tsx
@@ -42,6 +42,13 @@ export default function TopicCreateForm() {
               isInvalid={!!formState.errors.description}
               errorMessage={formState.errors.description?.join(', ')}
             />
+
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(', ')}
+              </div>
+            ) : null}
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
