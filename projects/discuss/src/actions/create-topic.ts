eb56e5aac1f29da9a5edc17b29e5078ab5570241
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-24 13:40:24 +0100

    feat: implement topic creation form  with validation in server action

--- projects/discuss/src/actions/create-topic.ts
+++ projects/discuss/src/actions/create-topic.ts
@@ -1,5 +1,42 @@
 'use server';
 
-export async function createTopic() {
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  return {
+    errors: {},
+  };
+
   // TODO: revalidate the homepage
 }
