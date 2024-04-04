77675fd05b762178afaf4b379f042e034eb7cbbc
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-04-04 08:45:23 +0200

    feat: topic show page scaffolding

--- projects/discuss/src/app/topics/[slug]/page.tsx
+++ projects/discuss/src/app/topics/[slug]/page.tsx
@@ -1,3 +1,19 @@
-export default function TopicShowPage() {
-  return <div>Topic Show</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
