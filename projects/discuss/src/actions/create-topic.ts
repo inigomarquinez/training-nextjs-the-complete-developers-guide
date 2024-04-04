d1b146037b32d354584f274fa392461fc5905d9a
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-04-04 08:30:59 +0200

    feat: show loading spinners with useFormStatus

--- projects/discuss/src/actions/create-topic.ts
+++ projects/discuss/src/actions/create-topic.ts
@@ -30,6 +30,9 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
+  // Simulate network latency
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
